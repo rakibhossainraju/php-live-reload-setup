06e0890070a51467df82ae3e58493d37a2a40dfc
rakibhossainraju <rakibhossianraju@gmail.com>
2024-09-23 10:22:35 +0600

    Added new module open that will open browser on the specified url

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
-const gulp = require('gulp');
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({port: 3001});
+import gulp from "gulp";
+import {WebSocketServer} from 'ws'; 
+import open from "open"
+
+const wss = new WebSocketServer({ port: 3001 });
 
 wss.on('connection', ws => {
     console.log('Client connected');
     ws.on('message', message => {
-      console.log('Received: %s', message);
+        console.log('Received: %s', message);
     });
-  });
-  
+});
+
 gulp.task('serve', () => {
-gulp.watch("**/*.php").on("change", () => {
-
-    console.log('PHP file changed, sending reload signal...');
-
-    wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN) {
-        client.send('reload'); 
-    }
+    // Watch for PHP file changes
+    gulp.watch("**/*.php").on("change", () => {
+        console.log('PHP file changed, sending reload signal...');
+        
+        // Send reload signal to WebSocket clients
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send('reload'); 
+            }
+        });
     });
 
+    // Automatically open 'hello-world.test' in the browser
+    open('http://hello-world.test');  // This will open the browser to the specified URL
 });
-});
-  
-gulp.task("default", gulp.series("serve"));
+
+gulp.task('default', gulp.series('serve'));
